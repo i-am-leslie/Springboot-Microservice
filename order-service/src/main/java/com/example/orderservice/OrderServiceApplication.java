05134d7444035bd7664061e5be2c76475eed611c
ejehleslie <ejehboss@gmail.com>
2025-01-17 21:11:30 -0500

    Kafka and spring cloud stream

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,5 +15,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 public class OrderServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@Bean
+	public Consumer<String> stringConsumer() {
+		return message -> System.out.println("Consumed message: " + message);  // Data consumed by the Subscriber
 	}
 }
--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -3,16 +3,25 @@ package com.example.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 @SpringBootApplication(scanBasePackages = "com.example.productservice")
 @EnableDiscoveryClient
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
+	@Bean
+	public Supplier<String> stringSupplier() {
+		return () -> "Hello from Publisher (Supplier)";  // Data produced by the Publisher
+	}
 }
