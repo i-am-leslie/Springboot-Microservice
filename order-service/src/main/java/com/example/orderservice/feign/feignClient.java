b4752f7220107b10296c397110ff03ff29752322
ejehleslie <ejehboss@gmail.com>
2025-01-22 22:47:11 -0500

    Working on deploying

--- order-service/src/main/java/com/example/orderservice/feign/feignClient.java
+++ order-service/src/main/java/com/example/orderservice/feign/feignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("PRODUCT-SERVICE")
+@FeignClient(value = "PRODUCT-SERVICE", url = "${product-service.url}")
 public interface feignClient {
     @GetMapping(value = "/v1/product/getProductById/{id}")
     String getProductById(@PathVariable("id") String id);
