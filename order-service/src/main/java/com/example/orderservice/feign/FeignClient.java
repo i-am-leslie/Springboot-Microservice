85b449e6a7624468d1501e43ca2750f75f73a472
ejehleslie <ejehboss@gmail.com>
2025-02-07 15:20:39 -0500

    configurartion improved

--- order-service/src/main/java/com/example/orderservice/feign/FeignClient.java
+++ order-service/src/main/java/com/example/orderservice/feign/FeignClient.java
@@ -4,7 +4,7 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@org.springframework.cloud.openfeign.FeignClient(value = "PRODUCT-SERVICE", url = "${product-service.url}")
+@org.springframework.cloud.openfeign.FeignClient(value = "PRODUCT-SERVICE")
 public interface FeignClient {
     @GetMapping(value = "/v1/product/getProductById/{id}")
     @CircuitBreaker(name="order-service")
